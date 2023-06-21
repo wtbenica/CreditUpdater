ce9006cba0ab56a6ba40d1827f4089ff63847878
wesley <wesley@benica.dev>
2023-06-21 02:47:11 -0700

    added some actual database hit tests to QueryExecutorTest;

--- src/test/kotlin/dev/benica/creditupdater/db/QueryExecutorTest.kt
+++ src/test/kotlin/dev/benica/creditupdater/db/QueryExecutorTest.kt
@@ -1,30 +1,16 @@
 package dev.benica.creditupdater.db
 
+import dev.benica.creditupdater.Credentials.Companion.PASSWORD_INITIALIZER
+import dev.benica.creditupdater.Credentials.Companion.TEST_DATABASE
+import dev.benica.creditupdater.Credentials.Companion.USERNAME_INITIALIZER
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.mockito.Mockito.*
 import java.io.File
-import java.sql.Connection
-import java.sql.ResultSet
-import java.sql.Statement
+import java.sql.*
 
 class QueryExecutorTest {
-    private lateinit var queryExecutor: QueryExecutor
-    private lateinit var connection: Connection
-    private lateinit var statement: Statement
-    private lateinit var resultSet: ResultSet
-
-    @BeforeEach
-    fun setUp() {
-        connection = mock(Connection::class.java)
-        statement = mock(Statement::class.java)
-        resultSet = mock(ResultSet::class.java)
-
-        queryExecutor = QueryExecutor("A string")
-    }
-
     @Test
     @DisplayName("should parse script correctly")
     fun shouldParseSqlScript() {
@@ -57,61 +43,247 @@ class QueryExecutorTest {
         assertEquals(expected, actual)
     }
 
-    /*
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
-import java.sql.Connection
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-import java.sql.Statement
-
-class SqlQueryExecutorTest {
-    private lateinit var sqlQueryExecutor: SqlQueryExecutor
-    private lateinit var connectionSource: ConnectionSource
-    private lateinit var connection: Connection
-    private lateinit var statement: Statement
-    private lateinit var preparedStatement: PreparedStatement
-    private lateinit var resultSet: ResultSet
-
-    @BeforeEach
-    fun setUp() {
-        connectionSource = mock(ConnectionSource::class.java)
-        connection = mock(Connection::class.java)
-        statement = mock(Statement::class.java)
-        preparedStatement = mock(PreparedStatement::class.java)
-        resultSet = mock(ResultSet::class.java)
-
-        `when`(connectionSource.getConnection()).thenReturn(connection)
-        `when`(connection.createStatement()).thenReturn(statement)
-        `when`(connection.prepareStatement("INSERT INTO table (column1, column2) VALUES (?, ?)")).thenReturn(preparedStatement)
-        `when`(preparedStatement.executeUpdate()).thenReturn(1)
-        `when`(statement.executeQuery("SELECT COUNT(*) FROM table")).thenReturn(resultSet)
-        `when`(resultSet.next()).thenReturn(true)
-        `when`(resultSet.getInt(1)).thenReturn(1)
-
-        sqlQueryExecutor = SqlQueryExecutor(connectionSource)
+    @AfterEach
+    fun tearDown() {
     }
 
     @Test
-    @DisplayName("should insert row into database")
-    fun shouldInsertRowIntoDatabase() {
-        val expected = 1
-        val actual = sqlQueryExecutor.executeSqlStatement("INSERT INTO table (column1, column2) VALUES (?, ?)", listOf("value1", "value2"))
+    @DisplayName("Should execute CREATE TABLE")
+    fun shouldExecuteCreateTable() {
+        val tableName = "test_table_create"
+        val sqlStmt = "CREATE TABLE $tableName (id INT PRIMARY KEY);"
 
-        assertEquals(expected, actual)
+        queryExecutor.executeSqlStatement(sqlStmt)
 
-        // Check that the row was inserted into the database
-        val countStatement = connection.createStatement()
-        val countResultSet = countStatement.executeQuery("SELECT COUNT(*) FROM table")
-        countResultSet.next()
-        val rowCount = countResultSet.getInt(1)
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/$TEST_DATABASE", USERNAME_INITIALIZER, PASSWORD_INITIALIZER
+        ).use { conn ->
+            val metadata = conn.metaData
+            val table = metadata.getTables(null, null, tableName, null)
 
-        assertEquals(1, rowCount)
+            assertTrue(table.next(), "Table $tableName should exist")
+        }
     }
-}
- */
+
+    @Test
+    @DisplayName("Should execute ALTER TABLE")
+    fun shouldExecuteAlterTable() {
+        val tableName = "test_table_alter"
+        val columnName = "test_column"
+        val sqlStmt = "ALTER TABLE $tableName ADD COLUMN $columnName VARCHAR(255);"
+
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/$TEST_DATABASE", USERNAME_INITIALIZER, PASSWORD_INITIALIZER
+        ).use { conn ->
+            // Create test table
+            val createTableStmt = "CREATE TABLE $tableName (id INT PRIMARY KEY);"
+            conn.createStatement().use { stmt ->
+                stmt.execute(createTableStmt)
+            }
+
+            // Execute ALTER TABLE
+            queryExecutor.executeSqlStatement(sqlStmt)
+
+            // Check that column exists
+            val metadata = conn.metaData
+            val table = metadata.getColumns(null, null, tableName, columnName)
+            assertTrue(table.next(), "Column $columnName should exist in table $tableName")
+        }
+    }
+
+    @Test
+    @DisplayName("Should execute INSERT INTO")
+    fun shouldExecuteInsertInto() {
+        val tableName = "test_table_insert_into"
+        val sqlStmt = "INSERT INTO $tableName VALUES (1);"
+
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/$TEST_DATABASE", USERNAME_INITIALIZER, PASSWORD_INITIALIZER
+        ).use { conn ->
+            // Create test table
+            val createTableStmt = "CREATE TABLE $tableName (id INT PRIMARY KEY);"
+            conn.createStatement().use { stmt ->
+                stmt.execute(createTableStmt)
+            }
+
+            // Execute INSERT INTO
+            queryExecutor.executeSqlStatement(sqlStmt)
+
+            // Check that row exists
+            val selectStmt = "SELECT * FROM $tableName WHERE id = 1;"
+            conn.createStatement().use { stmt ->
+                val resultSet = stmt.executeQuery(selectStmt)
+                assertTrue(resultSet.next(), "Row with id 1 should exist in table $tableName")
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Should execute UPDATE")
+    fun shouldExecuteUpdate() {
+        val tableName = "test_table_update"
+        val sqlStmt = "UPDATE $tableName SET id = 2 WHERE id = 1;"
+
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/$TEST_DATABASE", USERNAME_INITIALIZER, PASSWORD_INITIALIZER
+        ).use { conn ->
+            // Create test table
+            val createTableStmt = "CREATE TABLE $tableName (id INT PRIMARY KEY);"
+            conn.createStatement().use { stmt ->
+                stmt.execute(createTableStmt)
+            }
+
+            // Insert row
+            val insertStmt = "INSERT INTO $tableName VALUES (1);"
+            conn.createStatement().use { stmt ->
+                stmt.execute(insertStmt)
+            }
+
+            // Execute UPDATE
+            queryExecutor.executeSqlStatement(sqlStmt)
+
+            // Check that row exists
+            val selectStmt = "SELECT * FROM $tableName WHERE id = 2;"
+            conn.createStatement().use { stmt ->
+                val resultSet = stmt.executeQuery(selectStmt)
+                assertTrue(resultSet.next(), "Row with id 2 should exist in table $tableName")
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Should execute DELETE")
+    fun shouldExecuteDelete() {
+        val tableName = "test_table_delete"
+        val sqlStmt = "DELETE FROM $tableName WHERE id = 1;"
+
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/$TEST_DATABASE", USERNAME_INITIALIZER, PASSWORD_INITIALIZER
+        ).use { conn ->
+            // Create test table
+            val createTableStmt = "CREATE TABLE $tableName (id INT PRIMARY KEY);"
+            conn.createStatement().use { stmt ->
+                stmt.execute(createTableStmt)
+            }
+
+            // Insert row
+            val insertStmt = "INSERT INTO $tableName VALUES (1);"
+            conn.createStatement().use { stmt ->
+                stmt.execute(insertStmt)
+            }
+
+            // Execute DELETE
+            queryExecutor.executeSqlStatement(sqlStmt)
+
+            // Check that row exists
+            val selectStmt = "SELECT * FROM $tableName WHERE id = 1;"
+            conn.createStatement().use { stmt ->
+                val resultSet = stmt.executeQuery(selectStmt)
+                assertTrue(!resultSet.next(), "Row with id 1 should not exist in table $tableName")
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Should execute DROP TABLE")
+    fun shouldExecuteDropTable() {
+        val tableName = "test_table_drop"
+        val sqlStmt = "DROP TABLE $tableName;"
+
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/$TEST_DATABASE", USERNAME_INITIALIZER, PASSWORD_INITIALIZER
+        ).use { conn ->
+            // Create test table
+            val createTableStmt = "CREATE TABLE $tableName (id INT PRIMARY KEY);"
+            conn.createStatement().use { stmt ->
+                stmt.execute(createTableStmt)
+            }
+
+            // Execute DROP TABLE
+            queryExecutor.executeSqlStatement(sqlStmt)
+
+            // Check that table does not exist
+            val metadata = conn.metaData
+            val table = metadata.getTables(null, null, tableName, null)
+            assertTrue(!table.next(), "Table $tableName should not exist")
+        }
+    }
+
+    @Test
+    @DisplayName("Should throw SQLException when executing SELECT statement")
+    fun shouldThrowSQLExceptionWhenExecutingSelectStatement() {
+        val sqlStmt = "SELECT * FROM test_table;"
+
+        assertThrows<SQLException> {
+            queryExecutor.executeSqlStatement(sqlStmt)
+        }
+    }
+
+    @Test
+    fun executeQueryAndDo() {
+    }
+
+    @Test
+    fun executeSqlScript() {
+    }
+
+    @Test
+    fun `parseSqlScript$CreditUpdater`() {
+    }
+
+    @Test
+    fun `getItemCount$CreditUpdater`() {
+    }
+
+    @Test
+    fun executePreparedStatementBatch() {
+    }
+
+    @Test
+    fun executePreparedStatement() {
+    }
+
+    companion object {
+        private lateinit var queryExecutor: QueryExecutor
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+            queryExecutor = QueryExecutor("credit_updater_test")
+
+            dropAllTables()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun breakDown() {
+            dropAllTables()
+        }
+
+        private fun dropAllTables() {
+            DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/credit_updater_test", USERNAME_INITIALIZER, PASSWORD_INITIALIZER
+            ).use { conn ->
+                val query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'credit_updater_test'"
+                conn.createStatement().use { stmt ->
+                    // Retrieve the names of all tables in the database
+                    stmt.executeQuery(query).use { resultSet ->
+                        val tableNames = mutableListOf<String>()
+
+                        // Store the table names in a list
+                        while (resultSet.next()) {
+                            val tableName = resultSet.getString("table_name")
+                            tableNames.add(tableName)
+                        }
+
+                        // Generate and execute DROP TABLE statements for each table
+                        tableNames.forEach { tableName ->
+                            val dropStatement = "DROP TABLE $tableName"
+                            stmt.executeUpdate(dropStatement)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
